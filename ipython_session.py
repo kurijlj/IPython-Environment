b07e09bceb736f5217baf72ae873811fc2c2d1ab
Ljubomir Kurij <kurijlj@gmail.com>
2017-11-28 22:43:13 +0100

    20171128-224310

---
+++ ipython_session.py
@@ -0,0 +1,32 @@
+# IPython log file
+
+get_ipython().run_line_magic('logstart', './ipython_session.py')
+get_ipython().run_line_magic('matplotlib', '')
+get_ipython().run_line_magic('load_ext', 'autoreload')
+get_ipython().run_line_magic('autoreload', '2')
+from matplotlib import pyplot as plt
+from nmrgraphs import *
+fig1, ax1 = plt.subplots()
+t1_relax_graph(ax1, 8, 1, 1, 2)
+ax1.cla()
+ytl = list()
+for item in ax1.get_yticklabels():
+    text = item.get_text()
+    if '\u22121.00' == text:
+        text = r'$-M_0$'
+    elif '1.00' == text:
+        text = r'$M_0$'
+    else:
+        text = r''
+    ytl.append(text)
+ax1.set_yticklabels(ytl)
+quit()
+get_ipython().run_line_magic('logstart', './ipython_session.py append')
+get_ipython().run_line_magic('matplotlib', '')
+get_ipython().run_line_magic('load_ext', 'autoreload')
+get_ipython().run_line_magic('autoreload', '2')
+from matplotlib import pyplot as plt
+from nmrgraphs import *
+fig1, ax1 = plt.subplots()
+t1_relax_graph(ax1, 10, 1, 1, 2)
+quit()
--- nmrgraphs.py
+++ nmrgraphs.py
@@ -7,15 +7,34 @@ def t1_relax_graph(ax, rng, amp, toa, tob):
     y1 = np.array([amp*(1 - (2 * m.exp(-(t/toa)))) for t in x])
     y2 = np.array([amp*(1 - (2 * m.exp(-(t/tob)))) for t in x])
 
-    ax.plot(x, y1, label='$T_{1a}$', color='#a4c122')
-    ax.plot(x, y2, label='$T_{1b}$', color='#c122a4')
+    ax.plot(x, y1, label='$T_{1a}$', color='#a4c122', linewidth=3)
+    ax.plot(x, y2, label='$T_{1b}$', color='#c122a4', linewidth=3)
 
     ax.set_xlabel(r'Vreme [$t/T_1$]')
     ax.set_ylabel(r'Longitudinalna magnetizacija $M_z$')
 
-    ax.axhline(-1, 0, 1, ls='dashed', label=r'$-M_0$')
-    ax.axhline(1, 0, 1, ls='dashed', label=r'$+M_0$')
+    ax.axhline(-1, 0, 1, ls='dashed')
+    ax.axhline(1, 0, 1, ls='dashed')
 
     ax.grid('on')
     ax.minorticks_on()
+    #ytl = list()
+
+    #for item in ax.get_yticklabels():
+    #    text = item.get_text()
+    #    print(text)
+    #    if '\u22121.00' == text:
+    #        text = r'$-M_0$'
+    #    elif '1.00' == text:
+    #        text = r'$M_0$'
+    #    else:
+    #        text = r''
+    #    ytl.append(text)
+
+    ytl = [r'', r'$-M_0$', r'', r'', r'', r'', r'', r'', r'', r'$M_0$', r'']
+    ax.set_yticklabels(ytl)
+
+    xlims = ax.get_xlim()
+    ax.set_xlim(xlims[0], rng)
+
     ax.legend()
