2ed191cc53e1c972df41aeb28f20d196d32e0cc0
Ljubomir Kurij <kurijlj@gmail.com>
2017-11-30 22:23:03 +0100

    20171130-222258

--- nmrgraphs.py
+++ nmrgraphs.py
@@ -1,14 +1,19 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 import math as m
 
 
+def t1_relax(t, amp, to):
+    return amp*(1 - (2 * m.exp(-(t/to))))
+
+
 def t1_relax_graph(ax, rng, amp, toa, tob):
     x = np.arange(0, rng, 0.05)
-    y1 = np.array([amp*(1 - (2 * m.exp(-(t/toa)))) for t in x])
-    y2 = np.array([amp*(1 - (2 * m.exp(-(t/tob)))) for t in x])
+    y1 = np.array([t1_relax(t, amp, toa) for t in x])
+    y2 = np.array([t1_relax(t, amp, tob) for t in x])
 
     ax.set_xlabel(r'Vreme [$t/T_1$]')
-    ax.set_ylabel(r'Relaksacija longitudinalne magnetizacije $M_z$')
+    ax.set_ylabel(u'Relaksacija longitudinalne magnetizacije $M_z$')
 
     ax.axhline(-1, 0, 1, ls='dashed')
     ax.axhline(0, 0, 1, color='black')
@@ -35,22 +40,40 @@ def t1_relax_graph(ax, rng, amp, toa, tob):
     #    ytl.append(text)
 
     xtl = [r'', r'', r'', r'$TR_1$', r'', r'', r'', r'$TR_2$', r'', r'']
-    ytl = [r'', r'$-M_0$', r'', r'', r'', r'$0.0$', r'', r'', r'', r'$M_0$', r'']
+    ytl = [r'', r'$-M_0$', r'', r'', r'', r'$0.0$',
+           r'', r'', r'', r'$M_0$', r'']
     ax.set_xticklabels(xtl)
     ax.set_yticklabels(ytl)
 
     xlims = ax.get_xlim()
     ax.set_xlim(xlims[0], rng)
 
-
+    # This works, but lets make it a bit better
+    #ax.annotate('',
+    #            xy=(2, 0.75),
+    #            xytext=(2, 0.25),
+    #            arrowprops=dict(arrowstyle="<->"))
+    #ax.annotate("",
+    #            xy=(6, 0.88),
+    #            xytext=(6, 1.01),
+    #            arrowprops=dict(arrowstyle="<->"))
     ax.annotate('',
-                xy=(2, 0.75),
-		xytext=(2, 0.25),
-		arrowprops=dict(arrowstyle="<->"))
+                xy=(2, round(t1_relax(2, amp, toa), 2) + 0.02),
+                xytext=(2, round(t1_relax(2, amp, tob), 2) - 0.02),
+                arrowprops=dict(arrowstyle="<->"))
     ax.annotate("",
-                xy=(6, 0.88),
-		xytext=(6, 1.01),
-		arrowprops=dict(arrowstyle="<->"))
+                xy=(6, round(t1_relax(6, amp, toa), 2) + 0.02),
+                xytext=(6, round(t1_relax(6, amp, tob), 2) - 0.02),
+                arrowprops=dict(arrowstyle="<->"))
 
-    ax.text(2.1, 0.5, 'Dobar\nkontrast', weight='bold', size='large')
+    ax.text(2.0 + 0.1,
+            round(t1_relax(2, amp, tob) + (t1_relax(2, amp, toa) - t1_relax(2, amp, tob)) / 2, 2),
+            u'Dobar\nkontrast',
+            weight='bold',
+            size='large')
+    ax.text(6.0 + 0.1,
+            round(t1_relax(6, amp, tob) + (t1_relax(6, amp, toa) - t1_relax(6, amp, tob)) / 2, 2),
+            u'Loš\nkontrast',
+            weight='bold',
+            size='large')
     ax.legend()
