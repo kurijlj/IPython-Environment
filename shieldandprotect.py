7171cf9c65eaa988478789d0e51273e30e2e15c2
Ljubomir Kurij @ slackwarebox <kurijlj@gmail.com>
2018-10-28 23:01:29 +0100

    slackwarebox commit

--- shieldandprotect.py
+++ shieldandprotect.py
@@ -1,3 +1,5 @@
+from math import log10
+
 class TVLDataset(object):
     """An abstract base class to hold TVL values and material densities for
     the IAEA SRS47 protocol and NCRP151 protocol for a given material
@@ -181,19 +183,51 @@ class NCRC_TVLDataset(TVLDataset):
         return self._leakage[energy][1]
 
 
-class IAEAPrimaryBarrier(object):
+class IAEAPrimaryBarrierGoal(object):
     """
     """
 
     def __init__(
             self,
-            P: float=1.0,
-            d,
-            SAD,
-            W,
-            U,
-            T
+            goal: float=1.0,
+            distance: float=1.0,
+            sad: float=1.0,
+            workload: float=800.0,
+            usage: float=1.0,
+            occupancy: float=1.0,
+            material: IAEA_TVLDataset=iaea_concrete,
+            energy: float=6.0
         ):
+
+        self._goal = goal
+        self._distance = distance
+        self._sad = sad
+        self._workload = workload
+        self._usage = usage
+        self._occupancy = occupancy
+        self._material = material
+        self._energy = energy
+
+    def transmission(self):
+        """
+        """
+
+        numerator = goal * pow((distance + sad), 2)
+        denominator = workload * usage * occupancy
+        return numerator / denominator
+
+    def tvl_number(self):
+        """
+        """
+
+        return log10(1.0 / self.transmission)
+
+    def barrier_thickness(self):
+        """
+        """
+
+        return tvl_number * self._material.primary[self._energy]
+
 
 iaea_concrete = IAEA_TVLDataset(
         density=2.35,
